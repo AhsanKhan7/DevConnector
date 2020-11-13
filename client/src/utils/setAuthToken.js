134df83560364d87395f0e74b4299cd84545e5b9
AhsanKhan7 <innocent.ahsan12345@gmail.com>
2020-11-12 23:38:21 -0800

    register and login

---
+++ client/src/utils/setAuthToken.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+//  > Goal is to take the token of registered user if they registered if not return false
+const setAuthToken = (token) => {
+  if (token) {
+    axios.defaults.headers.common["x-auth-token"] = token;
+  } else {
+    delete axios.defaults.headers.common["x-auth-token"];
+  }
+};
+
+export default setAuthToken;
